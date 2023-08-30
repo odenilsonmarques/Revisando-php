05bd9a4eaac866f8939c7a8e1e8dd8ac33ba26cc
odenilsonmarques <odmarquesgm@gmail.com>
2023-08-30 08:59:45 -0300

    estudo da estrutura while

---
+++ modulo03/praticas/while01.php
@@ -0,0 +1,10 @@
+<?php
+
+$contador = 1;
+
+while($contador <= 10){
+
+    echo "olha o pente, olha o pente, olha o pente <br>";
+
+    $contador++;
+}
---
+++ modulo03/praticas/while02.php
@@ -0,0 +1,13 @@
+
+<select>
+    <option selected> --- Selecione --- </option>
+    <?php
+
+        $ano = 2022;
+
+        while($ano >= 1900){
+            echo "<option>{$ano}</option>";
+            $ano--;
+        }
+    ?>
+</select>
